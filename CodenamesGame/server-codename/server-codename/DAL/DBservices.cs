1dbb07865daca2257e2046efb4a762891c1b3bfb
Roee1997 <roee608@gmail.com>
2025-03-11 15:59:35 +0200

    setting up DAL and appsettings.json

---
+++ CodenamesGame/server-codename/server-codename/DAL/DBservices.cs
@@ -0,0 +1,25 @@
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace server-codname.DAL
+{
+    public class DBservices
+    {
+        private readonly string connectionString;
+
+        public DBservices()
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json").Build();
+            connectionString = configuration.GetConnectionString("CodenamesDB");
+        }
+
+        public SqlConnection Connect()
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            return con;
+        }
+    }
+}

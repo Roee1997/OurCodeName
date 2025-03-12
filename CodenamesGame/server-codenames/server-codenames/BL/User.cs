bad0e30756db61fef17b2656ff223bef71f98d0f
OsherGarufi <oshergarufi@gmail.com>
2025-03-12 11:06:33 +0200

    New ASP.Net
    הוא עשה לי בעיות אז מחקתי והקמתי חדש

---
+++ CodenamesGame/server-codenames/server-codenames/BL/User.cs
@@ -0,0 +1,37 @@
+﻿namespace server_codenames.BL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public List<int> Friends { get; set; } = new List<int>();
+
+        public User() { }
+
+        public User(string username, string email, string passwordHash)
+        {
+            Username = username;
+            Email = email;
+            PasswordHash = passwordHash;
+            RegistrationDate = DateTime.Now;
+        }
+
+        public void AddFriend(int friendId)
+        {
+            if (!Friends.Contains(friendId))
+                Friends.Add(friendId);
+        }
+
+        public void RemoveFriend(int friendId)
+        {
+            Friends.Remove(friendId);
+        }
+    }
+}
